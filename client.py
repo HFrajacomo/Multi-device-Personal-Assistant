139b8e56389cfd1eb7f4427f27a88b076bf1fa36
Henrique Frajacomo <henrique.frajacomo@gmail.com>
2020-02-09 04:47:31 -0300

    Added SpeechRecognition class
    Uses google default as default and sphinx in offline service

---
+++ client.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append('src\\')
+sys.path.append('src/')
+
+from NetSocket import *
+
+s = NetSocket()
+s.open_connection("192.168.0.30")
+
+s.send("Hello world")
+print(s.recv().decode())
+
+s.close()
---
+++ src/SpeechRecognition.py
@@ -0,0 +1,81 @@
+import speech_recognition as sr
+from threading import Lock
+from kthread import KThread
+from random import randint
+import os
+import sys
+
+from Path import Path
+
+class SpeechRecognition:
+	exception_phrases = ["What?", "I didn't undestand", "What did you say?", "What was that?"]
+
+	def __init__(self):
+		self.client = sr.Recognizer()
+		self.text = ""
+
+		self.LOCK = Lock()
+		self.EXIT = False
+		self.thread = KThread(target=self.audio_thread)
+		self.thread.start()
+
+	# Blocks until audio thread gets a text
+	def recv_text(self):
+		try:
+			while(1):
+				if(self.EXIT):
+					return
+
+				if(self.text == ""):
+					self.LOCK.acquire()
+					continue
+
+				recognized_text = self.text
+				self.text = ""
+				return recognized_text
+		except KeyboardInterrupt:
+			self.close()
+			sys.exit()
+
+	# KThreaded to recognize audio
+	def audio_thread(self):
+		while(1):
+			with sr.Microphone() as source:
+				print("Speak")
+				audio = self.client.listen(source)
+				try:
+					self.text = self.client.recognize_google(audio)
+					if(self.text == None):
+						self.text = self.__get_exception()
+				except:
+					try:
+						self.text = self.client.recognize_sphinx(audio)
+						if(self.text == None):
+							self.text = self.__get_exception()
+
+					except:
+						self.text = "Something is wrong with my Speech Recognition"
+
+				if(self.LOCK.locked()):
+					self.LOCK.release()
+
+	# Closes all thread activity
+	def close(self):
+		self.thread.kill()
+		self.EXIT = True
+
+		if(self.LOCK.locked()):
+			self.LOCK.release()
+
+	# Gets a random exception phrase
+	def __get_exception(self):
+		return self.exception_phrases[randint(0,len(self.exception_phrases)-1)]
+
+microfone = SpeechRecognition()
+try:
+	while(1):
+		text = microfone.recv_text()
+		print(text)
+except KeyboardInterrupt:
+	microfone.close()
+	sys.exit()
